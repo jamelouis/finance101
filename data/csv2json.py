22eb01983ea5d19a06a121101c6b035c6fc3f42f
jamelouis <jamelouis2015@gmail.com>
2023-08-01 23:41:11 +0800

    update data

---
+++ data/csv2json.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import json
+
+df = pd.read_csv('transactions.csv')
+print(df.head())
+data = []
+for index,item in df.iterrows():
+    data.append({
+        "date": item.date,
+        "type": item.type,
+        "name": item.name,
+        "code": item.Code,
+        "unitPrice": item.unitPrice if not pd.isnull(item.unitPrice) else None,
+        "quantity": item.quantity if not pd.isnull(item.quantity) else None,
+        "fee": item.fee if not pd.isnull(item.fee) else None,
+        "value": item.value if not pd.isnull(item.value) else None 
+    })
+
+transactions = {
+    "name": "etf150",
+    "data": data 
+}
+with open('transactions.json', 'w', encoding='utf-8') as fp:
+    json.dump(transactions, fp, indent=2)
