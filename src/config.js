9b8f532a22235ed9077fa5edab44e90b4b32fc6b
jamelouis <jamelouis2015@gmail.com>
2023-08-02 08:16:18 +0800

    update config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const owner = process.env.owner || "jamelouis";
 export const repo = process.env.repo || "finance101";
 export const portfolioPath = process.env.portfolioPath || "data/portfolios";
-export const token = process.env.token || "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";
+export const token = process.env.token || "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";
